3fe6c0fab554b3c3ac71d784ce80114863e9aa35
Fay <xufayflying@gmail.com>
2013-09-24 11:22:43 +0800

    finish the main part of theme one

---
+++ wp-content/themes/themeone/searchform.php
@@ -0,0 +1,4 @@
+<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<input type="text" class="field" name="s" id="s" placeholder="搜索文章" />
+	<input type="submit" class="submit" name="submit" id="searchsubmit" value="" />
+</form>
