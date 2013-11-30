f33824fba00e923aeba5d6247d965621e0b135ae
Fay <xufayflying@gmail.com>
2013-11-30 15:03:23 +0800

    update to 3.7.1

--- wp-config.php
+++ wp-config.php
@@ -1,29 +1,29 @@
 <?php
-/** 
- * WordPress 基础配置文件。
+/**
+ * WordPress基础配置文件。
  *
- * 本文件包含以下配置选项：MySQL 设置、数据库表名前缀、密钥、
- * WordPress 语言设定以及 ABSPATH。如需更多信息，请访问
+ * 本文件包含以下配置选项：MySQL设置、数据库表名前缀、密钥、
+ * WordPress语言设定以及ABSPATH。如需更多信息，请访问
  * {@link http://codex.wordpress.org/zh-cn:%E7%BC%96%E8%BE%91_wp-config.php
- * 编辑 wp-config.php} Codex 页面。MySQL 设置具体信息请咨询您的空间提供商。
+ * 编辑wp-config.php}Codex页面。MySQL设置具体信息请咨询您的空间提供商。
  *
- * 这个文件用在于安装程序自动生成 wp-config.php 配置文件，
- * 您可以手动复制这个文件，并重命名为“wp-config.php”，然后输入相关信息。
+ * 这个文件被安装程序用于自动生成wp-config.php配置文件，
+ * 您可以手动复制这个文件，并重命名为“wp-config.php”，然后填入相关信息。
  *
  * @package WordPress
  */
 
 // ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
-/** WordPress 数据库的名称 */
+/** WordPress数据库的名称 */
 define('DB_NAME', 'flyslow_local');
 
-/** MySQL 数据库用户名 */
+/** MySQL数据库用户名 */
 define('DB_USER', 'root');
 
-/** MySQL 数据库密码 */
+/** MySQL数据库密码 */
 define('DB_PASSWORD', '');
 
-/** MySQL 主机 */
+/** MySQL主机 */
 define('DB_HOST', 'localhost');
 
 /** 创建数据表时默认的文字编码 */
@@ -33,55 +33,65 @@ define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
 /**#@+
- * 身份认证密匙设定。
+ * 身份认证密钥与盐。
  *
- * 您可以随意写一些字符
- * 或者直接访问 {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org 私钥生成服务}，
- * 任何修改都会导致 cookie 失效，所有用户必须重新登录。
+ * 修改为任意独一无二的字串！
+ * 或者直接访问{@link https://api.wordpress.org/secret-key/1.1/salt/
+ * WordPress.org密钥生成服务}
+ * 任何修改都会导致所有cookies失效，所有用户将必须重新登录。
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         'b-?w~PQQ`!$F*R<RNynG7i7r-mC0a%1.1Ng+-<ar*|G^(n,+ONZ7390L^*8_!mXr');
+define('SECURE_AUTH_KEY',  '-q/V|FwlnnR9/UwB E=nXDlML-{d%&u?%Ll[T-++% Sv%bw7:5wTCH?;R`c3=!z|');
+define('LOGGED_IN_KEY',    'r{-g]EuvN[UH@{(u2s<|d+}vi~]C/5:7.wn!tjBB3[r[7X/TO/9W=4@LF?-j2v^G');
+define('NONCE_KEY',        'B.,E;7%RAJ=$J=p:@D5=:{-T!+?+s^g5^mv+*1V]M6A3WbA=E?|>g =jFl19m(42');
+define('AUTH_SALT',        'oAwkjA}qp*nwAYJAzC H9Q<9dYO_06d/d?WW^A0_>|Ja_%oeQ[&LWd:|Rcn2}*T=');
+define('SECURE_AUTH_SALT', 'rTJ6zXkPEY8|?YFo#,%;V;+7+Kar$Io%QPvg=W+NYwgI??>V-Q&@,7gA~z>ey=[w');
+define('LOGGED_IN_SALT',   'nt[-d.JoL|3db%8sv4pMG?{|MIQ|LR5IJRVH{[2eJ|-8sB^UrOrHLf>t]]&;C;uu');
+define('NONCE_SALT',       '4[wE-Z-@z*UUUl|ai%Rf@<753%a,B!GV^hhb,j|GsgrDSD8L_|mKIi7]f-Y=M&1F');
 
 /**#@-*/
 
 /**
- * WordPress 数据表前缀。
+ * WordPress数据表前缀。
  *
- * 如果您有在同一数据库内安装多个 WordPress 的需求，请为每个 WordPress 设置不同的数据表前缀。
- * 前缀名只能为数字、字母加下划线。
+ * 如果您有在同一数据库内安装多个WordPress的需求，请为每个WordPress设置
+ * 不同的数据表前缀。前缀名只能为数字、字母加下划线。
  */
 $table_prefix  = 'wp_';
 
 /**
- * WordPress 语言设置，中文版本默认为中文。
+ * WordPress语言设置，中文版本默认为中文。
  *
- * 本项设定能够让 WordPress 显示您需要的语言。
- * wp-content/languages 内应放置同名的 .mo 语言文件。
- * 要使用 WordPress 简体中文界面，只需填入 zh_CN。
+ * 本项设定能够让WordPress显示您需要的语言。
+ * wp-content/languages内应放置同名的.mo语言文件。
+ * 例如，要使用WordPress简体中文界面，请在wp-content/languages
+ * 放入zh_CN.mo，并将WPLANG设为'zh_CN'。
  */
 define('WPLANG', 'zh_CN');
 
 /**
- * 开发者专用：WordPress 调试模式。
+ * 开发者专用：WordPress调试模式。
  *
- * 将这个值改为“true”，WordPress 将显示所有用于开发的提示。
- * 强烈建议插件开发者在开发环境中启用本功能。
+ * 将这个值改为true，WordPress将显示所有用于开发的提示。
+ * 强烈建议插件开发者在开发环境中启用WP_DEBUG。
  */
-define('WP_DEBUG', true);
+define('WP_DEBUG', false);
+
+/**
+ * zh_CN本地化设置：启用ICP备案号显示
+ *
+ * 可在设置→常规中修改。
+ * 如需禁用，请移除或注释掉本行。
+ */
+define('WP_ZH_CN_ICP_NUM', true);
 
 /* 好了！请不要再继续编辑。请保存本文件。使用愉快！ */
 
-/** WordPress 目录的绝对路径。 */
+/** WordPress目录的绝对路径。 */
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
 
-/** 设置 WordPress 变量和包含文件。 */
+/** 设置WordPress变量和包含文件。 */
 require_once(ABSPATH . 'wp-settings.php');
--- wp-content/themes/themeone/searchform.php
+++
@@ -1,4 +0,0 @@
-<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<input type="text" class="field" name="s" id="s" placeholder="搜索文章" />
-	<input type="submit" class="submit" name="submit" id="searchsubmit" value="" />
-</form>
